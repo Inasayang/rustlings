2d42ac5c366cf80310957667a04c4caf5be79432
Inasa <me@inasa.dev>
2025-08-17 22:19:53 +0800

    00-04

---
+++ exercises/02_functions/functions1.rs
@@ -0,0 +1,10 @@
+// TODO: Add some function with the name `call_me` without arguments or a return value.
+
+fn call_me() {
+    // This function does nothing, but it exists to satisfy the exercise requirements.
+    println!("Function call_me has been called!");
+}
+
+fn main() {
+    call_me(); // Don't change this line
+}
